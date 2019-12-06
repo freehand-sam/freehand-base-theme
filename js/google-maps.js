ce79a7b8ec4f939ad9d0e0a866d69eee6bbb8f37
freehand-sam <samtoerper@gmail.com>
2019-12-06 17:18:49 -0600

    Reverted googlemap.js to my version

--- js/google-maps.js
+++ js/google-maps.js
@@ -1,64 +1,151 @@
+//ACF Google Map
 (function ($) {
-    let args = {
-        zoom: 18,
-        center: new google.maps.LatLng(0, 150),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        scrollwheel: false,
-        mapTypeControl: false,
-        streetViewControl: false,
-        styles: JSON.parse(wp.mapstyle)
-    };
 
-    // Setup New Google Map
-    function newMap($el, location) {
-        let map = new google.maps.Map($el[0], args);
+    /*
+    *  new_map
+    *
+    *  This function will render a Google Map onto the selected jQuery element
+    *
+    *  @type    function
+    *  @date    8/11/2013
+    *  @since   4.3.0
+    *
+    *  @param   $el (jQuery element)
+    *  @return  n/a
+    */
+
+
+    function new_map($el) {
+
+        // var
+        var $markers = $el.find('.marker'),
+            $mapstyles = JSON.parse(script_vars.mapstyles),
+            args = {
+                zoom: 18,
+                center: new google.maps.LatLng(32.7513994,-97.3643357),
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                scrollwheel: false,
+                streetViewControl: false,
+                styles: $mapstyles
+            };
+
+        // create map               
+        var map = new google.maps.Map($el[0], args);
+
+        // add a markers reference
         map.markers = [];
 
-        addMarker(location, map);
+         // add markers
+        $markers.each(function(){
+            add_marker( $(this), map );
+        });
 
-        centerMap(map);
+        // center map
+        center_map(map);
+
+        // return
         return map;
     }
 
-    // Add Google Map Marker
-    function addMarker(location, map) {
-        let latlng = new google.maps.LatLng(location.lat, location.lng);
-        let marker = new google.maps.Marker({
+    /*
+    *  add_marker
+    *
+    *  This function will add a marker to the selected Google Map
+    *
+    *  @type    function
+    *  @date    8/11/2013
+    *  @since   4.3.0
+    *
+    *  @param   $marker (jQuery element)
+    *  @param   map (Google Map object)
+    *  @return  n/a
+    */
+
+    function add_marker($marker, map) {
+        // var
+        var latlng = new google.maps.LatLng($marker.attr('data-lat'), $marker.attr('data-lng')),
+            icon = $marker.attr('data-icon');
+
+        // create marker
+        var marker = new google.maps.Marker({
             position: latlng,
             map: map,
             animation: google.maps.Animation.DROP,
-            icon: wp.poimarker
+            icon: icon
         });
 
+        // add to array
         map.markers.push(marker);
+
+        // if marker contains HTML, add it to an infoWindow
+        if ($marker.html()) {
+            // create info window
+            var infowindow = new google.maps.InfoWindow({
+                content: $marker.html(),
+            });
+
+
+            // show info window when marker is clicked
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.open(map, marker);
+            });
+        }
     }
 
-    // Center Google Map
-    function centerMap(map, zoom = 16) {
-        let bounds = new google.maps.LatLngBounds();
-        let visibile = 0;
+    /*
+    *  center_map
+    *
+    *  This function will center the map, showing all markers attached to this map
+    *
+    *  @type    function
+    *  @date    8/11/2013
+    *  @since   4.3.0
+    *
+    *  @param   map (Google Map object)
+    *  @return  n/a
+    */
 
-        map.markers.forEach(marker => {
-            if (marker.visible) {
-                bounds.extend(marker.position);
-                visibile++;
-            }
+    function center_map(map) {
+        // vars
+        var bounds = new google.maps.LatLngBounds();
+
+        // loop through all markers and create bounds
+        $.each(map.markers, function (i, marker) {
+            var latlng = new google.maps.LatLng(marker.position.lat(), marker.position.lng());
+            bounds.extend(latlng);
         });
 
-        if (map.markers.length == 1 || visibile == 1) {
+        // only 1 marker?
+        if (map.markers.length == 1) {
+            // set center of map
             map.setCenter(bounds.getCenter());
-            map.setZoom(zoom);
+            map.setZoom(14);
         } else {
+            // fit to bounds
             map.fitBounds(bounds);
         }
     }
 
-    $('.acf-map').each((i, e) => {
-        let location = {
-            lng: $(e).data('lng'),
-            lat: $(e).data('lat')
-        }
+    /*
+    *  document ready
+    *
+    *  This function will render each map when the document is ready (page has loaded)
+    *
+    *  @type    function
+    *  @date    8/11/2013
+    *  @since   5.0.0
+    *
+    *  @param   n/a
+    *  @return  n/a
+    */
+    // global var
+    var map = null;
 
-        newMap($(e), location);
-    })
+    $(document).ready(function () {
+        $('.acf-map').each(function () {
+            // create map
+            map = new_map($(this));
+        });
+     });
+
 })(jQuery);
