fc2f3a69be949bbb4dde5f16a774657be1d9cb75
freehand-sam <sam@freehand.studio>
2019-12-06 22:45:05 -0600

    Update enqueue.php
    Rearranged order of scripts loading

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -24,6 +24,8 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 
         wp_enqueue_script( 'jquery' );
         
+        wp_enqueue_script('aos', 'https://unpkg.com/aos@next/dist/aos.js', array('understrap-scripts'), 1.0, true );
+        
         $gm_api = get_field('google_maps_api_key','option');
         if ($gm_api) {
             wp_enqueue_script('googlemaps', 'https://maps.googleapis.com/maps/api/js?key=' . $gm_api, null, null, true);
@@ -31,10 +33,11 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
             wp_localize_script('googlemaps_config', 'script_vars', array('mapstyles' => get_field('map_stylers', 'option')));
         }
 
-        wp_enqueue_script('aos', 'https://unpkg.com/aos@next/dist/aos.js', array(''), 1.0, true );
-
 		$js_version = $theme_version . '.' . filemtime( get_template_directory() . '/js/theme.min.js' );
-		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array('aos'), $js_version, true );
+		
+		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $js_version, true );
+        
+		
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
