6e20f671ae41269dea86421415cf116ba5902886
freehand-sam <sam@freehand.studio>
2019-12-06 18:24:27 -0600

    Updated AOS script

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -31,10 +31,10 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
             wp_localize_script('googlemaps_config', 'script_vars', array('mapstyles' => get_field('map_stylers', 'option')));
         }
 
-        wp_enqueue_script('aos', 'https://unpkg.com/aos@next/dist/aos.js', array('understrap-scripts'), 1.0, true );
+        wp_enqueue_script('aos', 'https://unpkg.com/aos@next/dist/aos.js', array(''), 1.0, true );
 
 		$js_version = $theme_version . '.' . filemtime( get_template_directory() . '/js/theme.min.js' );
-		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $js_version, true );
+		wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array('aos'), $js_version, true );
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
