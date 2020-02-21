2b35ac4b13f32d88830812518763a1086d70e962
Sam <sam@freehand.studio>
2020-02-21 14:03:59 -0600

    Fixed missing function to solve fatal error

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -129,3 +129,29 @@ if ( ! function_exists( 'understrap_category_transient_flusher' ) ) {
 		delete_transient( 'understrap_categories' );
 	}
 }
+
+if ( ! function_exists( 'understrap_body_attributes' ) ) {
+	/**
+	 * Displays the attributes for the body element.
+	 */
+	function understrap_body_attributes() {
+		/**
+		 * Filters the body attributes.
+		 *
+		 * @param array $atts An associative array of attributes.
+		 */
+		$atts = array_unique( apply_filters( 'understrap_body_attributes', $atts = array() ) );
+		if ( ! is_array( $atts ) || empty( $atts ) ) {
+			return;
+		}
+		$attributes = '';
+		foreach ( $atts as $name => $value ) {
+			if ( $value ) {
+				$attributes .= sanitize_key( $name ) . '="' . esc_attr( $value ) . '" ';
+			} else {
+				$attributes .= sanitize_key( $name ) . ' ';
+			}
+		}
+		echo trim( $attributes ); // phpcs:ignore WordPress.Security.EscapeOutput
+	}
+}
