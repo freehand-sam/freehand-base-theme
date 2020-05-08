327a6e08aa829a35c681674cb667278cc3d60e41
Sam <sam@freehand.studio>
2020-05-08 16:48:09 -0500

    Added footer logo and dynamic copyright to footer

--- footer.php
+++ footer.php
@@ -16,8 +16,12 @@ $container = get_theme_mod( 'understrap_container_type' );
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
 <footer class="site-footer" id="wrapper-footer">
+	<?php if(get_theme_mod('freehand_footer_logo') != ''): ?>
+	  <?php $footer_logo = esc_url(get_theme_mod('freehand_footer_logo')); ?>
+	  <img class="footer-logo" src="<?php echo $footer_logo; ?>" alt="<?php bloginfo( 'name' ); ?>" >
+	<?php endif; ?>
     <div class="site-info">
-        <?php understrap_site_info(); ?>
+        <?php echo dynamic_copyright() . ' '; bloginfo( 'name' ); ?> <span class="nowrap">All rights reserved.</span></a>
     </div>
 </footer>
 
